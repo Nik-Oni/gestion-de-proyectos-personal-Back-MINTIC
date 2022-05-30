00d6d96fa4b38bff9c2e5f6e808eb18d9b6c1f84
[Nikion] <[nigutjim@gmail.com]>
2022-05-30 10:23:20 -0500

    creacion de nueva branch

--- graphql/types.ts
+++ graphql/types.ts
@@ -1,108 +1,109 @@
-import {gql} from 'apollo-server-express'
+import { gql } from 'apollo-server-express';
 
 const typeDefs = gql`
+  scalar Date
 
-    scalar Date
+  enum Enum_EstadoUsuario {
+    PENDIENTE
+    AUTORIZADO
+    NO_AUTORIZADO
+  }
 
-    enum Enum_EstadoUsuario{
-        PENDIENTE
-        AUTORIZADO
-        NO_AUTORIZADO
-    }
+  enum Enum_Rol {
+    ESTUDIANTE
+    LIDER
+    ADMINISTRADOR
+  }
 
-    enum Enum_Rol{
-        ESTUDIANTE
-        LIDER
-        ADMINISTRADOR
-    }
+  enum Enum_EstadoProyecto {
+    ACTIVO
+    INACTIVO
+  }
 
-    enum Enum_EstadoProyecto{
-        ACTIVO
-        INACTIVO
-    }
+  enum Enum_FaseProyecto {
+    INICIADO
+    DESARROLLO
+    TERMINADO
+    NULO
+  }
 
-    enum Enum_FaseProyecto{
-        INICIADO
-        DESARROLLO
-        TERMINADO 
-        NULA
-    }
+  enum Enum_TipoObjetivo {
+    GENERAL
+    ESPECIFICO
+  }
 
-    enum Enum_TipoObjetivo {
-        GENERAL
-        ESPECIFICO
-    }
+  type Usuario {
+    _id: ID!
+    nombre: String!
+    apellido: String!
+    identificacion: String!
+    correo: String!
+    rol: Enum_Rol!
+    estado: Enum_EstadoUsuario
+  }
 
+  type Objetivo {
+    _id: ID!
+    descripcion: String!
+    tipo: Enum_TipoObjetivo!
+  }
 
-    type Usuario{
-        _id: ID!
-        nombre:String!
-        apellido: String!
-        identificacion: String!
-        correo: String!
-        estado: Enum_EstadoUsuario
-        rol: Enum_Rol!
-    }
+  input crearObjetivo {
+    descripcion: String!
+    tipo: Enum_TipoObjetivo!
+  }
 
-    type Objetivo{
-        _id:ID!
-        descripcion: String! 
-        tipo: Enum_TipoObjetivo!
-    }
+  type Proyecto {
+    _id: ID!
+    nombre: String!
+    presupuesto: Float!
+    fechaInicio: Date!
+    fechaFin: Date!
+    estado: Enum_EstadoProyecto!
+    fase: Enum_FaseProyecto!
+    lider: Usuario!
+    objetivos: [Objetivo]
+  }
 
-    input crearObjetivo{
-        descripcion: String! 
-        tipo: Enum_TipoObjetivo!
-    }
+  type Query {
+    Usuarios: [Usuario]
+    Usuario(_id: String!): Usuario
+    Proyectos: [Proyecto]
+  }
 
-    type Proyecto{
-        _id:ID!
-        nombre: String!
-        presupuesto: Float!
-        fechaInicio: Date!
-        fechaFin: Date!
-        estado: Enum_EstadoProyecto!
-        fase: Enum_FaseProyecto!
-        lider: Usuario!
-        objetivos:[Objetivo]!
-    }
-    type Query{
-        Usuarios: [Usuario]
-        Usuario(_id:String!): Usuario
-        Proyectos:[Proyecto]
-    }
+  type Mutation {
+    crearUsuario(
+      nombre: String!
+      apellido: String!
+      identificacion: String!
+      correo: String!
+      rol: Enum_Rol!
+      estado: Enum_EstadoUsuario
+    ): Usuario
 
-    type Mutation {
-        crearUsuario(
-            nombre:String!
-            apellido: String!
-            identificacion: String!
-            correo: String!
-            rol: Enum_Rol!
-        ): Usuario
+    editarUsuario(
+      _id: String!
+      nombre: String!
+      apellido: String!
+      identificacion: String!
+      correo: String!
+      rol: Enum_Rol!
+      estado: Enum_EstadoUsuario
+    ): Usuario
 
-        editarUsuario(
-            _id: String!
-            nombre:String!
-            apellido: String!
-            identificacion: String!
-            correo: String!
-            rol: Enum_Rol!
-            estado: Enum_EstadoUsuario
-        ) : Usuario
+    eliminarUsuario(_id: String, correo: String): Usuario
 
-        eliminarUsuario(_id: String!, correo: String): Usuario
-        
-        crearProyecto(
-            nombre: String!
-            presupuesto: Float!
-            fechaInicio: Date!
-            fechaFin: Date!
-            estado: Enum_EstadoProyecto!
-            fase: Enum_FaseProyecto!
-            lider: String!
-            objetivos: [crearObjetivo!]
-        ): Proyecto
-    }
+    crearProyecto(
+      nombre: String!
+      presupuesto: Float!
+      fechaInicio: Date!
+      fechaFin: Date!
+      estado: Enum_EstadoProyecto!
+      fase: Enum_FaseProyecto!
+      lider: String!
+      objetivos: [crearObjetivo]
+    ): Proyecto
+  }
 `;
-export {typeDefs}
+
+export { typeDefs };
