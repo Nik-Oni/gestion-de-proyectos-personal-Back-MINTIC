af5d15d14bca7e9781cb3af9041f7a02a9bdcbf1
[Nikion] <[nigutjim@gmail.com]>
2022-05-30 08:52:10 -0500

    Error despliegue heroku, copia repositorio Daniel

--- models/avances.ts
+++ models/avances.ts
@@ -1,41 +1,41 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model } from 'mongoose';
 import { ProjectModel } from './project';
 import { UserModel } from './user';
 
 interface Avance {
-    fecha: Date;
-    descripcion: string;
-    observaciones: [string];
-    proyecto: Schema.Types.ObjectId;
-    creadoPor: Schema.Types.ObjectId;
+  fecha: Date;
+  descripcion: string;
+  observaciones: [string];
+  proyecto: Schema.Types.ObjectId;
+  creadoPor: Schema.Types.ObjectId;
 }
 
 const advancementSchema = new Schema<Avance>({
-    fecha: {
-        type: Date,
-        required: true,
+  fecha: {
+    type: Date,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+  },
+  observaciones: [
+    {
+      type: String,
     },
-    descripcion: {
-        type: String,
-        required: true
-    },
-    observaciones: [
-        {
-            type: String,
-        },
-    ],
-    proyecto: {
-        type: Schema.Types.ObjectId,
-        ref: ProjectModel,
-        required: true,
-    },
-    creadoPor: {
-        type: Schema.Types.ObjectId,
-        ref: UserModel,
-        required: true,
-    }
-})
+  ],
+  proyecto: {
+    type: Schema.Types.ObjectId,
+    ref: ProjectModel,
+    required: true,
+  },
+  creadoPor: {
+    type: Schema.Types.ObjectId,
+    ref: UserModel,
+    required: true,
+  },
+});
 
-const AdvancementModel = model('Modelo', advancementSchema)
+const AdvancementModel = model('Avance', advancementSchema);
 
-export { AdvancementModel }
+export { AdvancementModel };
